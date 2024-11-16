2dc6700c6e21a1d87b33742ceaf664fcaf03ca69
meghan9908 <gmeghankrishna@gmail.com>
2024-11-16 19:08:10 +0530

    callbackUri_Change

--- src/app/libs/nylas.ts
+++ src/app/libs/nylas.ts
@@ -3,7 +3,7 @@ import Nylas from "nylas";
 
 export const nylasConfig = {
   clientId: process.env.NYLAS_CLIENT_ID ?? (() => { throw new Error("Missing NYLAS_CLIENT_ID") })(),
-  callbackUri: "http://localhost:3000/api/oauth/exchange",
+  callbackUri: `${process.env.NXT_PUBLIC_URL}/api/oauth/exchange`,
   apiKey: process.env.NYLAS_API_KEY ?? (() => { throw new Error("Missing NYLAS_API_KEY") })(),
   apiUri: process.env.NYLAS_API_URI ?? "https://api.nylas.com", // default Nylas API URI
 };
