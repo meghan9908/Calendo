aaa36c7ad0a93647d16cedcac966abe3564ca3cc
meghan9908 <gmeghankrishna@gmail.com>
2024-11-17 14:03:50 +0530

    cookie http:true

--- src/app/libs/session.ts
+++ src/app/libs/session.ts
@@ -1,23 +1,17 @@
 import nextAppSession from 'next-app-session';
 
 type MySessionData = {
-   grant_id?: string;
-   email?: string;
+  grantId?: string;
+  email?: string;
 };
 
 export const session = nextAppSession<MySessionData>({
-   name: 'calendix_session',
-   secret: process.env.SECRET as string,
-   cookie: {
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
-      secure: process.env.NODE_ENV === 'production',
-      httpOnly: true,
-      sameSite: 'lax',
-   },
+  name: 'calendix_session',
+  secret: process.env.SECRET as string,
+  cookie: {
+    maxAge: 0, // Set cookie to expire immediately on destroy
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: false,
+    sameSite: 'lax',
+  }
 });
-
-// Debugging example:
-const userSession = session();
-await userSession.set('email', 'user@example.com');
-const email = await userSession.get('email');
-console.log('Session email:', email);
