26e2aa9baa8f9532705ef034bb1a485a01f145f4
meghan9908 <gmeghankrishna@gmail.com>
2024-11-17 14:08:25 +0530

    cookie http:true only

--- src/app/libs/session.ts
+++ src/app/libs/session.ts
@@ -9,9 +9,6 @@ export const session = nextAppSession<MySessionData>({
   name: 'calendix_session',
   secret: process.env.SECRET as string,
   cookie: {
-    maxAge: 0, // Set cookie to expire immediately on destroy
-    secure: process.env.NODE_ENV === 'production',
     httpOnly: false,
-    sameSite: 'lax',
   }
 });
