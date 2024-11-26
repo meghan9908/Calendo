95acab119be6a537fef44197c62819b7cf47b41f
meghan9908 <gmeghankrishna@gmail.com>
2024-11-26 21:51:54 +0530

    cookie configuration changed dashboard-5

--- src/app/(site)/features/page.tsx
+++ src/app/(site)/features/page.tsx
@@ -2,7 +2,7 @@ import { cookies } from 'next/headers';
 
 export default async function FeaturesPage() {
     const cookieStore = await cookies();
-    const sessionCookie = (await cookieStore).get("calendix_session");
+    const sessionCookie = (await cookieStore)?.get("calendix_session");
     const email =await sessionCookie?.value; // Extract the email from the cookie value if it exists
   
   return (
